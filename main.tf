5ed11c2833454350b199d5fea5438ec2571e0908
Umamaheswari-gude <umamaheswari.g@everest.engineering>
2026-02-25 16:30:04 +0530

    Add the terraform main file

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source   = "./modules/vpc"
+  vpc_cidr = var.vpc_cidr
+  vpc_name = var.vpc_name
+}
+
+module "igw" {
+  source   = "./modules/internet_gateway"
+  vpc_id   = module.vpc.vpc_id
+  igw_name = var.igw_name
+}
+
+module "subnet" {
+  source            = "./modules/subnet"
+  vpc_id            = module.vpc.vpc_id
+  subnet_cidr       = var.subnet_cidr
+  availability_zone = var.aws_az
+  subnet_name       = var.subnet_name
+}
+
+module "route_table" {
+  source              = "./modules/route_table"
+  vpc_id              = module.vpc.vpc_id
+  igw_id              = module.igw.igw_id
+  subnet_id           = module.subnet.subnet_id
+  route_table_name    = var.route_table_name
+}
+
+module "security_group" {
+  source                     = "./modules/security_group"
+  vpc_id                     = module.vpc.vpc_id
+  security_group_name        = var.security_group_name
+  security_group_description = var.security_group_description
+}
+
+module "ec2" {
+  source            = "./modules/ec2_resources"
+  ami_id            = var.ec2_ami_id
+  instance_type     = var.ec2_instance_type
+  subnet_id         = module.subnet.subnet_id
+  security_group_id = module.security_group.security_group_id
+  key_name          = var.ec2_key_name
+  volume_size       = var.ec2_volume_size
+  instance_name     = var.ec2_instance_name
+}
